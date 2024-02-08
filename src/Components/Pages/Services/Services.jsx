e34b03a63dd8c2bc8630185508e51a8a92a175ca
David Gichuru <davidgichuruofficial@gmail.com>
2024-02-08 18:52:06 +0300

    Rendered a functional hero section

--- src/Components/Pages/Services/Services.jsx
+++ src/Components/Pages/Services/Services.jsx
@@ -5,16 +5,6 @@ import Offer from './Components/Offer';
 export default function Services() {
   return (
     <>
-     {/* <section class="container-fluid bg-primary p-5 hero-header mb-5">
-        <div class="row py-5">
-            <div class="col-12 text-center">
-                <h1 class="display-1 text-white">About Us</h1>
-                <a href="" class="h4 text-white">Home</a>
-                <i class="far fa-circle text-white px-2"></i>
-                <a href="" class="h4 text-white">About</a>
-            </div>
-        </div>
-    </section> */}
       <ServicesSection/>
       <Pricing/>
       <Offer/>
--- src/Utilities/Layout.jsx
+++ src/Utilities/Layout.jsx
@@ -1,11 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Nav from "../Components/UI Components/Nav";
 import Footer from "../Components/UI Components/Footer";
+import HeroSection from "../Components/UI Components/HeroSection";
+import { useLocation } from 'react-router-dom';
 
 export default function Layout(){
+
+    const location = useLocation();
+    const currentLocation = location.pathname.replace('/', '')
+
     return(
         <>
             <Nav />
+            {currentLocation !== "" && <HeroSection currentLocation={currentLocation} />}
             <Outlet />
             <Footer />
         </>
