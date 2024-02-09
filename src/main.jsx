416cafcd7895641d5b82a91ad2efeb51d5b603cd
David Gichuru <davidgichuruofficial@gmail.com>
2024-02-09 14:36:28 +0300

    Set it to use an external file with all my route definitions so that the layout component can read the route definitions

--- src/main.jsx
+++ src/main.jsx
@@ -2,28 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'; 
 import Layout from './Utilities/Layout'
-import Home from './Components/Pages/Home/Home';
-import About from './Components/Pages/About/About';
-import Contact from './Components/Pages/Contact/Contact';
-import Services from './Components/Pages/Services/Services';
-import Error from './Components/Pages/Error'
+import { routes } from './Utilities/routesDefinitions';
 import './index.css';
 import 'animate.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Components/Pages/Home/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="Home" element={<Home />} />
-          <Route path="About" element={<About />} />
-          <Route path="Services" element={<Services />} />
-          <Route path="Contact" element={<Contact />} />
-          <Route path="*" element={<Error />} />
+          <Route index element={<Home/>}/>
+          {routes.map(route => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
         </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
-)
+);
