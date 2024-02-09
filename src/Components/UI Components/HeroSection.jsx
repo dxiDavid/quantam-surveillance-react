6aa85433ee8249c5c821305435c9db28dfe8ead6
David Gichuru <davidgichuruofficial@gmail.com>
2024-02-09 14:42:10 +0300

    Changed it to conditionally render a url or page not found messsage on the banner based on the path provided

--- src/Components/UI Components/HeroSection.jsx
+++ src/Components/UI Components/HeroSection.jsx
@@ -2,21 +2,29 @@ import { Link } from 'react-router-dom';
 import { Container, Row, Col } from 'react-bootstrap';
 import { FaCircleNotch } from "react-icons/fa";
 
-export default function HeroSection({ currentLocation }){
+export default function HeroSection({ currentLocation, definedRoutes = [] }) {
+  const isNotFound = !definedRoutes.includes(currentLocation);
+
   return (
     <section className="container-fluid bg-primary p-5 hero-header mb-5">
       <Container>
         <Row className="py-5">
           <Col className="text-center">
-            <h1 className="display-1 text-white">{currentLocation}</h1>
+            <h1 className="display-1 text-white">{isNotFound ? "Page not Found" : currentLocation}</h1>
             <div className="breadcrumb justify-content-center align-items-center fs-4">
-              <a href="/" className="text-white nav-link">Home</a>
-              <FaCircleNotch className="text-white mx-2"/>
-              <Link href={`/${currentLocation}`} className="text-white nav-link">{currentLocation}</Link>
+              {isNotFound ? (
+                <Link to="/" className="text-white btn btn-primary btn-lg">Back To Home</Link>
+              ) : (
+                <>
+                  <a href="/" className="text-white nav-link">Home</a>
+                  <FaCircleNotch className="text-white mx-2" />
+                  <Link href={`/${currentLocation}`} className="text-white nav-link">{currentLocation}</Link>
+                </>
+              )}
             </div>
           </Col>
         </Row>
       </Container>
     </section>
   );
-};
+}
