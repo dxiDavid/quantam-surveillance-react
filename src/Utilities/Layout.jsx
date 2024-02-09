e02a7f106bdbb2f4f2aa6096cb3c58194379b534
David Gichuru <davidgichuruofficial@gmail.com>
2024-02-09 14:47:27 +0300

    Updated it to map the defined array of route objects in the routeDefinitions file

--- src/Utilities/Layout.jsx
+++ src/Utilities/Layout.jsx
@@ -1,20 +1,22 @@
+import { useLocation } from 'react-router-dom';
 import { Outlet } from "react-router-dom";
 import Nav from "../Components/UI Components/Nav";
 import Footer from "../Components/UI Components/Footer";
 import HeroSection from "../Components/UI Components/HeroSection";
-import { useLocation } from 'react-router-dom';
+import { routes } from './routesDefinitions';
 
-export default function Layout(){
+export default function Layout() {
+  const location = useLocation();
+  const definedRoutes = routes.map(route => route.path.replace('/', ''));
 
-    const location = useLocation();
-    const currentLocation = location.pathname.replace('/', '')
-
-    return(
-        <>
-            <Nav />
-            {currentLocation !== "" && <HeroSection currentLocation={currentLocation} />}
-            <Outlet />
-            <Footer />
-        </>
-    )
+  return (
+    <>
+      <Nav />
+      {location.pathname !== "/" && location.pathname !== "/Home" && (
+        <HeroSection currentLocation={location.pathname.replace('/', '')} definedRoutes={definedRoutes} />
+      )}
+      <Outlet />
+      <Footer />
+    </>
+  );
 }
